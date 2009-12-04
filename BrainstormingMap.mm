cfe49eb00c86b2bc1863e020169c7bcd79e60c0c
Brandon Satrom <brandon.satrom@thoughtascent.com>
2009-12-04 10:07:38 -0600

    Aded brainstorming map

---
+++ BrainstormingMap.mm
@@ -0,0 +1,18 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1259936912964" ID="ID_1851320527" MODIFIED="1259942043153" TEXT="Keeping Architectures Relevant: Using strategic domain-driven design something, something, something">
+<node CREATED="1259939166751" ID="ID_172584322" MODIFIED="1259939171714" POSITION="right" TEXT="Language importance">
+<node CREATED="1259939175982" ID="ID_501052994" MODIFIED="1259939179659" TEXT="Cost of translation"/>
+<node CREATED="1259939180190" ID="ID_24023486" MODIFIED="1259939193288" TEXT="Getting Devs and biz on same page"/>
+<node CREATED="1259942542300" ID="ID_1519116725" MODIFIED="1259942551366" TEXT="dialoge example of the pain of translation"/>
+</node>
+<node CREATED="1259939195756" ID="ID_1738466863" MODIFIED="1259939215450" POSITION="right" TEXT="staying relevant as an architect"/>
+<node CREATED="1259939216708" ID="ID_1402960552" MODIFIED="1259939229687" POSITION="right" TEXT="should be expressed in code"/>
+<node CREATED="1259939230115" ID="ID_789096791" MODIFIED="1259939239455" POSITION="right" TEXT="UML was not intended to be a programming language"/>
+<node CREATED="1259939433217" ID="ID_1504754603" MODIFIED="1259939440861" POSITION="right" TEXT="examples of no longer being relevant">
+<node CREATED="1259939441713" ID="ID_1410584619" MODIFIED="1259939450468" TEXT="Square IS A Rectangle example"/>
+<node CREATED="1259939452039" ID="ID_717624765" MODIFIED="1259939459235" TEXT="C++ and .NET memory mgmt example"/>
+</node>
+<node CREATED="1259942529376" ID="ID_1636668656" MODIFIED="1259942535824" POSITION="right" TEXT="Living with the decisions that you make"/>
+</node>
+</map>
